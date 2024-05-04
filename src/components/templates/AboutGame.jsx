57d81f945591ce4b7c6cefe3ca416bfe7e03fbdc
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-05-04 20:02:43 +0700

    homepage

--- src/components/templates/AboutGame.jsx
+++ src/components/templates/AboutGame.jsx
@@ -11,7 +11,7 @@ function AboutGame() {
   };
   return (
     <div className="about-game">
-      <TitleIcon iconStyleCustom={style} handleClick={() => navigate("/homepage")} />
+      <TitleIcon iconStyleCustom={style} handleClick={() => navigate("/")} />
       <h4>
         RPS or Rock Paper Scissors is considered the oldest hand game in the world. In fact, the game dates all the way back to the Chinese Han Dynasty. This era began in 206 BC and ended in 220 AD. There are also accounts of this game in
         Japanese history.
