2c8a554732a96b366fe609db0078fec095909f6f
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-06-08 21:02:08 +0700

    revision error handling

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,33 +1,28 @@
 export const errorHandler = (error) => {
-  const alertValue = {
-    alertTitle: null,
-    alertMessage: null,
-    alertButton: null,
-  };
-
-  if (error.code === "ERR_NETWORK") {
-    alertValue.alertTitle = `${error.message}`;
-    alertValue.alertMessage = "Try refresh the page or contact the developer";
-    alertValue.alertButton = "RELOAD";
-  } else if (error.response.status) {
-    if (error.response.status === 401) {
-      alertValue.alertTitle = `${error.response.data.name} ( ${error.response.data.message} )`;
-      alertValue.alertMessage = "Please relog to continue using the app";
-      alertValue.alertButton = "RELOG";
-    } else if (error.response.status === 500 || error.response.status === 504) {
-      if (error.response.status === 500) {
-        alertValue.alertTitle = `${error.response.statusText}`;
-      } else if (error.response.status === 504) {
-        alertValue.alertTitle = `${error.message}`;
-      }
-      alertValue.alertMessage = "Try to reload the page or contact the developer";
-      alertValue.alertButton = "RELOAD";
-    } else {
-      alert(error);
-    }
-  } else {
-    alert(error);
+  switch (error.response?.status) {
+    case 401:
+      return {
+        alertTitle: error.response.data.name,
+        alertMessage: `${error.response.data.message}, please relog to continue using the app`,
+        alertButton: "RELOG",
+      };
+    case 500:
+      return {
+        alertTitle: "Server Error",
+        alertMessage: "An internal server error occurred.",
+        alertButton: "RELOAD",
+      };
+    case 504:
+      return {
+        alertTitle: "Gateway Timeout",
+        alertMessage: "The server did not respond in time.",
+        alertButton: "RELOAD",
+      };
+    default:
+      return {
+        alertTitle: "Error",
+        alertMessage: error.message,
+        alertButton: "RELOAD",
+      };
   }
-
-  return alertValue;
 };
